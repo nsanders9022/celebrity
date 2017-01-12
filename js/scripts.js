6b9d5f51cb58ca4ff9135125b266d60c4322bf9f
Katy Daviscourt and Nicole Sanders <nicole@>
2017-01-12 11:21:49 -0800

    almost finish page, just need to remove previous answer if you play again

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $("#questionnaire").submit(function(event){
+    
+    var gender = $("input:radio[name=gender]:checked").val();
+    var age = $("#age").val();
+    var personality = $("input:radio[name=personality]:checked").val();
+
+
+    if (age === "0-15 years old") {
+      alert("You're too young to be dating!")
+    } else {
+      if (gender === "female") {
+        if (age === "16-25 years old" && personality === "mysterious") {
+          $(".cara").show();
+        } else if (age === "16-25 years old" && personality === "fun") {
+          $(".selena").show();
+        } else if (age === "26-45 years old" && personality === "mysterious") {
+          $(".eva").show();
+        } else if (age === "26-45 years old" && personality === "fun") {
+          $(".jennifer").show();
+        } else if (age === "45+" && personality === "fun") {
+          $(".meryl").show();
+        } else if (age === "45+" && personality === "mysterious") {
+          $(".queen").show();
+        }
+      } else if (gender === "male") {
+        if (age === "16-25 years old" && personality === "mysterious") {
+          $(".nick").show();
+        } else if (age === "16-25 years old" && personality === "fun") {
+          $(".thomas").show();
+        } else if (age === "26-45 years old" && personality === "mysterious") {
+          $(".chris").show();
+        } else if (age === "26-45 years old" && personality === "fun") {
+          $(".neil").show();
+        } else if (age === "45+" && personality === "fun") {
+          $(".tom").show();
+        } else if (age === "45+" && personality === "mysterious") {
+          $(".george").show();
+        }
+      }
+    }
+    event.preventDefault();
+  });
+});
